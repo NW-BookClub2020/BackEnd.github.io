e3cab7f1427c0004bf38fe0a7127352c4fe19410
Carlos <carlosrema2@gmail.com>
2020-07-08 18:24:06 -0500

    Adding table category

--- models/review.js
+++ models/review.js
@@ -2,18 +2,37 @@ const mysql = require('./connection');
 
 exports.insert = async (auction_obj) => {
     const connection = await mysql.connect();
-    const [data] = await connection.query(`INSERT INTO bookclub SET ?`, auction_obj);
+    const [data] = await connection.query(`INSERT INTO reviews SET ?`, auction_obj);
     return data;
 }
 
 exports.getById = async (id) => {
     const connection = await mysql.connect();
-    const [data] = await connection.query(`SELECT * FROM bookclub WHERE id = ?`, id);
+    const [data] = await connection.query(`SELECT * FROM reviews WHERE id = ?`, id);
     return data[0];
 }
 
 exports.update = async (auction_obj) => {
     const connection = await mysql.connect();
-    const [data] = await connection.query(`UPDATE bookclub SET ? WHERE id = ?`, [auction_obj, auction_obj.id]);
+    const [data] = await connection.query(`UPDATE reviews SET ? WHERE id = ?`, [auction_obj, auction_obj.id]);
+    return data;
+}
+
+
+exports.insert = async (auction_obj) => {
+    const connection = await mysql.connect();
+    const [data] = await connection.query(`INSERT INTO user SET ?`, auction_obj);
+    return data;
+}
+
+exports.getById = async (id) => {
+    const connection = await mysql.connect();
+    const [data] = await connection.query(`SELECT * FROM user WHERE id = ?`, id);
+    return data[0];
+}
+
+exports.update = async (auction_obj) => {
+    const connection = await mysql.connect();
+    const [data] = await connection.query(`UPDATE user SET ? WHERE id = ?`, [auction_obj, auction_obj.id]);
     return data;
 }
